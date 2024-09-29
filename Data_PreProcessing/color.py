11cc57276285e08e44beaa5d58df9d1c51e709e2
amspsingh04 <sp.singh.150604@gmail.com>
2024-09-29 18:55:57 +0530

    .

--- Data_PreProcessing/color.py
+++ Data_PreProcessing/color.py
@@ -3,7 +3,8 @@ import os
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
 
-
+input_directory='../Dataset/training/Bleeding/d369e4f163df4aba_2101.jpg'
+output_directory='../Dataset_new/training/Bleeding'
 # Define the directory path
 input_1= '../Dataset/training'
 input_2='../Dataset/validation'
@@ -11,16 +12,17 @@ output_1 = '../Dataset_new/training'
 output_2 = '../Dataset_new/validation'
 
 if not (os.path.exists(output_1) and os.path.exists(output_2)):
-    os.makedirs(output_1)
-    os.makedirs(output_2)
+    os.makedirs(output_directory)
 
-def sepia(image):
-    sepia_filter = np.array([[0.272, 0.534, 0.131],
-                             [0.349, 0.686, 0.168],
-                             [0.393, 0.769, 0.189]])
-    sepia_image = cv2.transform(image, sepia_filter)
-    sepia_image = np.clip(sepia_image, 0, 255).astype(np.uint8)
-    return sepia_image
+#def sepia(image):
+#   sepia_filter = np.array([[0.272, 0.534, 0.131],
+#                           [0.349, 0.686, 0.168],
+#                          [0.393, 0.769, 0.189]])
+# sepia_image = cv2.transform(image, sepia_filter)
+# sepia_image = np.clip(sepia_image, 0, 255).astype(np.uint8)
+# return sepia_image
+#We use the above one to sepia our images
+#I do not think we need to sepia our images, but we can use the above template to provide a color filter over the images (we can use it to grayscale images)
 
 def sharpenn(image):
     kernel = np.array([[0, -1, 0],
@@ -28,14 +30,17 @@ def sharpenn(image):
                    [0, -1, 0]])
     filtered_image = cv2.filter2D(image, -1, kernel)
     return filtered_image
+#check sharpened image example here: '..\examples\sharpen.jpg' and decide whether to use
 
 def adjust_brightness(image, brightness=30):
     bright_image = cv2.convertScaleAbs(image, beta=brightness)
     return bright_image
+#check brightness example here: '..\examples\brightness.jpg' and toggle around to decide on parameters
 
 def adjust_contrast(image, contrast=1.5):
     adjusted_image = cv2.convertScaleAbs(image, alpha=contrast, beta=0)
     return adjusted_image
+#check contrast toggling here: ''..\examples\contrast.jpg'' and decide on parameters
 
 def apply_filter(image):
     #image = sepia(image)
